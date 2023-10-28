dd32b4265383cee486a044319583afa012c29b81
Sergey Golubev <skyandy2796@yandex.ru>
2023-10-29 01:15:21 +0300

    Научился выводить картинки. С помощью map выводить несколько элементов

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,16 +1,27 @@
 import styles from './Home.module.css'
+import {cars} from './cars.data.js'
 function Home() {
     return (
         <div>
             <h1>Каталог машин</h1>
             <div className='catalog'>
-                <div className={styles.item}>
-                    <h2>Car 1</h2>
-                    <p>100 000$</p>
-                    <button>Подробнее</button>
-                </div>
+                {cars.map(car => (
+                    <div key={car.id} className={styles.item}>
+                        <div className={styles.image} style={{backgroundImage: `url(${car.image})`}}></div>
+                        <div className={styles.info}>
+                            <h2>{car.name}</h2>
+                            <p>{new Intl.NumberFormat('en-US',{
+                                style: 'currency',
+                                currency: "USD",
+                            }).format(car.price)}</p>
+                            <button>Подробнее</button>
+                        </div>
+                    </div>
+                ))}
+
             </div>
         </div>
     )
 }
+
 export default Home
---
+++ src/components/screens/home/cars.data.js
@@ -0,0 +1,27 @@
+export const cars = [
+    {
+        id: 1,
+        name: 'BMV 3',
+        price: 24970,
+        image: './car1.jpg'
+    },
+    {
+        id: 2,
+        name: 'BMV 4',
+        price: 134567,
+        image: './car2.jpg'
+    },
+    {
+        id: 3,
+        name: 'BMV 5',
+        price: 2497240,
+        image: './car3.jpg'
+    },
+    {
+        id: 4,
+        name: 'BMV 6',
+        price: 2497110,
+        image: './car4.jpg'
+    }
+
+]
